2fb405aab4332f6aa0e78fbe09237338240a09f3
BillChan226 <chen4399@purdue.edu>
2023-11-20 11:27:04 +0800

    clarify early exit layers

--- minigpt4/models/eva_vit.py
+++ minigpt4/models/eva_vit.py
@@ -463,10 +463,11 @@ def create_eva_vit_g(img_size=224,drop_path_rate=0.4,use_checkpoint=False,precis
         use_checkpoint=use_checkpoint,
         early_exit_layers=early_exit_layers,
     )  
-    url = "https://storage.googleapis.com/sfr-vision-language-research/LAVIS/models/BLIP2/eva_vit_g.pth"
-    cached_file = download_cached_file(
-        url, check_hash=False, progress=True
-    )
+    # url = "https://storage.googleapis.com/sfr-vision-language-research/LAVIS/models/BLIP2/eva_vit_g.pth"
+    # cached_file = download_cached_file(
+    #     url, check_hash=False, progress=True
+    # )
+    cached_file = "/data/xyq/bill/models/eva_vit_g.pth"
     state_dict = torch.load(cached_file, map_location="cpu")    
     interpolate_pos_embed(model,state_dict)
     
